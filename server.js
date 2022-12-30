e59d3f6e1fdb7d22a53d0525cdc332f2d01f8cb1
taminhnhat <taminhnhat98@gmail.com>
2022-12-30 13:54:30 +0700

    i

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose')
 const stockController = require('./Controller/stock.controller')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+    .catch(err => {
+        console.log(err)
+    })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
