5ac5a39402f28207e337adebf3c7558d61a0eef5
taminhnhat <taminhnhat98@gmail.com>
2023-02-07 10:39:10 +0700

    i

---
+++ middleware/validation.js
@@ -0,0 +1,12 @@
+const validator = require('express-validator')
+
+exports.validate = (method) => {
+    switch (method) {
+        case 'putToLight':
+            break
+        case 'pickToLight':
+            break
+        default:
+            break;
+    }
+}
