cd438c7583ec20b63628024986e48c40252c3a3e
taminhnhat <taminhnhat98@gmail.com>
2023-06-08 16:31:41 +0700

    i

--- src/services/wall-gateway.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo echo '' > /var/log/mergewall/gateway.log
-sudo chmod 744 /var/log/mergewall/gateway.log
-sudo node /home/ubuntu/Supplier-Return-App/server.js > /var/log/mergewall/gateway.log
